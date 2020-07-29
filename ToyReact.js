a1c796122f55e3682fbe651bc6aa21ef5a546274
lilin <LiLin@datagrand.com>
2020-07-29 17:26:32 +0800

    feat(life-cycle):添加生命周期

--- ToyReact.js
+++ ToyReact.js
@@ -4,13 +4,31 @@ class ElementWrapper {
     this.root = document.createElement(type);
   }
   setAttribute(name, value) {
+    // 绑定事件
+    if (name.match(/^on([\s\S]+)$/)) {
+      let eventName = RegExp.$1.replace(/^[\s\S]/, (s) => s.toLocaleLowerCase());
+      this.root.addEventListener(eventName, value);
+    }
+    if (name === 'className') {
+      name = 'class';
+    }
     this.root.setAttribute(name, value);
   }
+  // 实dom中如果有虚的child，也需要使用range来进行操作
   appendChild(vChild) {
-    vChild.mountTo(this.root);
+    let range = document.createRange();
+    if (this.root.children.length) {
+      range.setStartAfter(this.root.lastChild);
+      range.setEndAfter(this.root.lastChild);
+    } else {
+      range.setStart(this.root, 0);
+      range.setEnd(this.root, 0);
+    }
+    vChild.mountTo(range);
   }
-  mountTo(parent) {
-    parent.appendChild(this.root);
+  mountTo(range) {
+    range.deleteContents();
+    range.insertNode(this.root);
   }
 }
 // 创建text文本节点
@@ -18,31 +36,74 @@ class TextWrapper {
   constructor(content) {
     this.root = document.createTextNode(content);
   }
-  mountTo(parent) {
-    parent.appendChild(this.root);
+  mountTo(range) {
+    range.deleteContents();
+    range.insertNode(this.root);
   }
 }
 // 创建自定义组件
 export class Component {
   constructor() {
     this.children = [];
+    this.props = Object.create(null);
   }
   setAttribute(name, value) {
+    if (name.match(/^on([\s\S]+)$/)) {
+      let eventName = RegExp.$1.replace(/^[\s\S]/, (s) => s.toLocaleLowerCase());
+      // this.root.addEventListener(eventName, value);
+    }
+    // 自定义组件需设置props属性
+    this.props[name] = value;
     this[name] = value;
   }
-  mountTo(parent) {
-    const vdom = this.render();
-    vdom.mountTo(parent);
+  mountTo(range) {
+    this.range = range;
+    this.update();
   }
+  // 使用range更新试图
+  update() {
+    let placeholder = document.createComment('placeholder');
+    let range = document.createRange();
+    range.setStart(this.range.endContainer, this.range.endOffset);
+    range.setEnd(this.range.endContainer, this.range.endOffset);
+    range.insertNode(placeholder);
+
+    this.range.deleteContents();
+    const vdom = this.render();
+    vdom.mountTo(this.range);
+
+    // placeholder.parentNode.removeChild(placeholder);
+  }
+  // 虚dom中，children实际上是类的一个属性，可以在render中决定children的渲染位置
   appendChild(child) {
     this.children.push(child);
+  }
+  setState(state) {
+    let merge = (oldState, newState) => {
+      for (const key in newState) {
+        if (typeof newState[key] === 'object') {
+          if (typeof oldState[key] !== 'object') {
+            oldState[key] = {};
+          }
+          merge(oldState[key], newState[key]);
+        } else {
+          oldState[key] = newState[key];
+        }
+      }
+    }
+    if (!this.state && state) {
+      this.state = {};
+    }
+    merge(this.state, state);
+    console.log(this.state);
+    this.update();
   }
 }
 
 export const ToyReact = {
   // creatElement创建一个元素，当在js中定义一个标签元素时，就会调用该方法(webpack配置)
   creatElement(type, attributes, ...children) {
-    console.log(arguments);
+    // console.log(arguments);
     let element;
     // type为string,则表示创建的是原生标签
     if (typeof type === 'string') {
@@ -80,6 +141,14 @@ export const ToyReact = {
   },
   // render将创建的元素添加到另一个元素上
   render(vdom, element) {
-    vdom.mountTo(element);
+    let range = document.createRange();
+    if (element.children.length) {
+      range.setStartAfter(element.lastChild);
+      range.setEndAfter(element.lastChild);
+    } else {
+      range.setStart(element, 0);
+      range.setEnd(element, 0);
+    }
+    vdom.mountTo(range);
   }
 }
--- main.js
+++ main.js
@@ -1,18 +1,6 @@
 
 import { ToyReact, Component } from './ToyReact';
 
-class MyComponent extends Component {
-  render() {
-    return <div>
-      <span>Hello</span>
-      <span>Word</span>
-      <div>
-        {true}
-        {this.children}</div>
-    </div>;
-  }
-
-}
 // 将一个实体dom渲染到body上
 // const a = <div name='a' id='ida'>
 //   <span>Hello</span>
@@ -22,9 +10,71 @@ class MyComponent extends Component {
 // document.body.appendChild(a);
 
 // 将虚拟dom渲染到body上
-const b = <MyComponent name='MyComponent' id='idMyComponent'>
-  <div>child</div>
-</MyComponent >;
-ToyReact.render(b,
-  document.body
-);
+// class MyComponent extends Component {
+//   render() {
+//     return <div>
+//       <span>Hello</span>
+//       <span>Word</span>
+//       <div>
+//         {true}
+//         {this.children}</div>
+//     </div>;
+//   }
+
+// }
+// const b = <MyComponent name='MyComponent' id='idMyComponent'>
+//   <div>child</div>
+// </MyComponent >;
+// ToyReact.render(b,
+//   document.body
+// );
+
+class Square extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+  render() {
+    return (
+      <button className="square" onClick={() => this.setState({ value: 'X' })}>
+        {this.state.value || ''}
+      </button>
+    )
+  }
+}
+
+class Board extends Component {
+  renderSquare(i) {
+    return (
+      <Square
+        value={i}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+const c = <Board />
+ToyReact.render(c, document.body);
